d35895cf1125de9ebe64345547e8fe6e659b11ef
blushell <micheal.jones89@gmail.com>
2024-11-22 02:12:06 -0500

    gh pages test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 export default {
+	content: [
+		'./components/**/*.{js,vue,ts}',
+		'./layouts/**/*.vue',
+		'./pages/**/*.vue',
+		'./plugins/**/*.{js,ts}',
+		'./app.vue',
+		'./error.vue',
+	],
 	theme: {
 		extend: {
 			colors: {
