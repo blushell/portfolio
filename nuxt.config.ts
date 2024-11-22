59e32c96775551c56ac2e304bf77e0392e14e5cf
blushell <micheal.jones89@gmail.com>
2024-11-22 01:49:46 -0500

    gh pages test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
 	ssr: false,
+	NUXT_APP_BASE_URL: '/portfolio/',
 	modules: ['@nuxtjs/tailwindcss'],
 	css: ['@/assets/css/main.css', '@mdi/font/css/materialdesignicons.min.css'],
 	app: {
